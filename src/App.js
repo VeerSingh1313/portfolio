e04c058a9d9359673e201e504e2a844d3cf90868
Vishal <code@insteptechnologies.com>
2024-04-04 18:13:22 +0530

    4-4-2024

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './component/header';
 import Home from './component/home';
 import 'aos/dist/aos.css';
-import MyComponent from './component/about';
-import Education from './component/Education';
+// import MyComponent from './component/about';
+// import Education from './component/Education';
 import Contact from './component/contact';
 function App() {
   return (
     <>
       <Header />
       <Home />
-      <MyComponent />
-      <Education />
+      {/* <MyComponent />
+      <Education /> */}
       <Contact />
     </>
   );
